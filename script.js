030f27c143c601ef1056b16a2bbce7d2d8e04710
NickName73 <egorooz73@gmail.com>
2022-06-10 15:41:02 +0300

    Fun

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+///Redirect code
 const delay = ms => new Promise(res => setTimeout(res, ms));
 const redirectTo = async (url, waitTime) => {
     waitTime = waitTime * 1000;
@@ -5,15 +6,28 @@ const redirectTo = async (url, waitTime) => {
     console.log("Waited: " + waitTime);
     location.href = url;
 };
+const hashCode = function(s) {
+    if (s == null) return -1; 
+    var h = 0, l = s.length, i = 0;
+    if ( l > 0 )
+      while (i < l)
+        h = (h << 5) - h + s.charCodeAt(i++) | 0;
+    return h;
+};
 
 var link = window.location.href;
 var url = new URL(link);
 var sender = url.searchParams.get("sender");
 var to = url.searchParams.get("to");
-var wait = url.searchParams.get("wait");
-console.log(sender);
-console.log(to);
-console.log(wait);
+var wait = url.searchParams.get("wait"); 
+/// TODO: Add specific link preset ?preset=ncksite&wait=0,8
+
+///Image Code
+var nekoImage = document.getElementById("link_img");
+var linkText = document.getElementById("link_text");
+var linkTextLeft = document.getElementById("link_text_left");
+nekoImage.src = `https://count.getloli.com/get/@nitisredir:${sender}^${hashCode(to)}?theme=gelbooru34`;
+
 
 if (to != null) {
     if (sender != null) {
@@ -28,6 +42,9 @@ if (to != null) {
     try {
         redirect = new URL(to);
         if (redirect != null) {
+            linkText.textContent = redirect.host;
+            linkTextLeft.textContent = "Redirect to:";
+            linkText.href = redirect;
             redirectTo(redirect, wait);
         }
     } catch {
